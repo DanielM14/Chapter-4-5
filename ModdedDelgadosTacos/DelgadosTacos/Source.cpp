230ed4ef2a4213762c1c31003bcd3d26e7f6d22b
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-04-12 13:33:11 -0500

    update

--- ModdedDelgadosTacos/DelgadosTacos/Source.cpp
+++ ModdedDelgadosTacos/DelgadosTacos/Source.cpp
@@ -20,24 +20,8 @@ void printMenu()
 	std::cout << "Faijta                        3.00" << std::endl;
 	std::cout << "**********************************" << std::endl;
 }
-
-int main()
+void tacoTruck()
 {
-	double tacos = 0.99;
-	double burrtios = 1.99;
-	double enchiladas = 2.99;
-	double tostadas = 1.50;
-	double drink = 0.99;
-	double churro = 1.25;
-	double sumpremeBurrtio = 4.50;
-	double potatosOles = 1.99;
-	double quesadilla = 1.00;
-	double faijta = 3.00;
-	double taxRate = 0.7;
-	double tacosNumber = 0, burritoNumber = 0, enchiladasNumber = 0, tostadasNumber = 0 , drinkNumber = 0, churroNumber = 0, sumpremeBurrtioNumber = 0, potatoOlesNumber = 0, quesadillaNumber = 0, faijtaNumber = 0;
-	double total;
-	std::string userChoice;
-	
 	std::cout << "************Welcome to Delgados Tacos***************\n\n\n\n\n\n" << std::endl;
 	std::cout << " _____________________________________________ " << std::endl;
 	std::cout << " |                                           | " << std::endl;
@@ -52,6 +36,25 @@ int main()
 	std::cout << " |                                                      | " << std::endl;
 	std::cout << " |                                                      | " << std::endl;
 	std::cout << " -------({ $ })---------------------------({ $ })-------- " << std::endl;
+}
+int main()
+{
+	double tacos = 0.99;
+	double burritos = 1.99;
+	double enchiladas = 2.99;
+	double tostadas = 1.50;
+	double drink = 0.99;
+	double churro = 1.25;
+	double sumpremeBurrtio = 4.50;
+	double potatosOles = 1.99;
+	double quesadilla = 1.00;
+	double faijta = 3.00;
+	double taxRate = 0.7;
+	double tacosNumber = 0, burritoNumber = 0, enchiladasNumber = 0, tostadasNumber = 0 , drinkNumber = 0, churroNumber = 0, sumpremeBurrtioNumber = 0, potatoOlesNumber = 0, quesadillaNumber = 0, faijtaNumber = 0;
+	double total;
+	std::string userChoice, again;
+	
+	tacoTruck();
 	printMenu();
 	do{
 		std::cout << "Welcome to Delgados Tacos what item(s) woudld you like? >>>" << std::endl;
@@ -61,55 +64,68 @@ int main()
 		{
 			std::cout << "How many tacos do you want? " << std::endl;
 			std::cin >> tacosNumber;
+			total = tacos;
 		}
 		else if (userChoice == "burritos" || userChoice == "Burritos")
 		{
 			std::cout << "How many burritos do you want?" << std::endl;
 			std::cin >> burritoNumber;
+			total = burritos;
 		}
 		else if (userChoice == "enchiladas" || userChoice == "Enchiladas")
 		{
 			std::cout << "How many echiladas do you want?" << std::endl;
 			std::cin >> enchiladasNumber;
+			total = enchiladas;
 		}
 		else if (userChoice == "tostadas" || userChoice == "Tostadas")
 		{
 			std::cout << "How many tostadas would you like" << std::endl;
 			std::cin >> tostadasNumber;
+			total = tostadas;
 		}
 		else if (userChoice == "drink" || userChoice == "Drink")
 		{
 			std::cout << "How many drink's would you like?" << std::endl;
 			std::cin >> drinkNumber;
+			total = drink;
 		}
 		else if (userChoice == "churro" || userChoice == "Churro")
 		{
 			std::cout << "How many churro's do you want?" << std::endl;
 			std::cin >> churroNumber;
+			total = churro;
 		}
 		else if (userChoice == "sumpreme burrtio" || userChoice == "Sumpreme Burrtio")
 		{
 			std::cout << "How many sumpreme burrtios do you want?" << std::endl;
 			std::cin >> sumpremeBurrtioNumber;
+			total = sumpremeBurrtio;
 		}
 		else if (userChoice == "potato oles" || userChoice == "Potato Oles")
 		{
 			std::cout << "How many potato oles would you like?" << std::endl;
 			std::cin >> potatoOlesNumber;
+			total = potatosOles;
 		}
 		else if (userChoice == "quesodilla" || userChoice == "Quesodilla")
 		{
 			std::cout << "How many quesodillas would you like?" << std::endl;
 			std::cin >> quesadillaNumber;
+			total = quesadilla;
 		}
 		else if (userChoice == "fajita" || userChoice == "Fajita")
 		{
 			std::cout << "How many fajitas would you like?" << std::endl;
 			std::cin >> faijtaNumber;
+			total = faijta;
 		}
-	} 
-
+		std::cout << "Would you like to order anything else? Yes or No" << std::endl;
+		std::cin >> again;
+	} while (again != "No");
 	
+	std::cout << "Your total is " << total << " thank you come again." << std::endl;
+
 	system("pause");
 	return 0;
 		
