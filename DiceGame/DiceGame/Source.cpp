4625fa1993422b3fc751e819b7984691c900291a
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-04-16 13:17:54 -0500

    update

--- DiceGame/DiceGame/Source.cpp
+++ DiceGame/DiceGame/Source.cpp
@@ -2,10 +2,25 @@
 // Date: 4-16-2018
 // Auther: Daniel McGlasson
 
+#include <iostream>
+#include <string>
+
 int main()
 {
-	
+	std::string userChoice;
 
+	do{
+		std::cout << "Would you like to roll a di? >>>" << std::endl;
+		std::cin >> userChoice;
+			userChoice;
+		if (userChoice == "Yes" || userChoice == "yes")
+		{
+			
+		}
+		else if (userChoice == "No" || userChoice == "no")
+		{
+			std::cout << "Go home then kid!!" << std::endl;
+		}
 
 
 
