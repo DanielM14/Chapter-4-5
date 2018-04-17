140d310b53bc8f3a0910cfe2bc07032b699c5fd3
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2018-04-17 12:54:13 -0500

    update

--- DiceGame/DiceGame/Source.cpp
+++ DiceGame/DiceGame/Source.cpp
@@ -12,30 +12,27 @@
 int main()
 {
 	int rand();
-	double rollDieOne = 1 - 6;
-	double rollDieTwo = 1 - 6;
+	double rollDieOne, rollDieTwo, totalDiceNumber;
 	std::string userChoice;
 	
 
 	do{
 		std::cout << "Would you like to roll some dice? >>>" << std::endl;
 		std::cin >> userChoice;
-			userChoice;
+		rollDieOne = rand() % 6 + 1;
+		rollDieTwo = rand() % 6 + 1;
 		if (userChoice == "Yes" || userChoice == "yes")
-		{
-			rollDieOne = rand() % 6 + 1;
-			rollDieTwo = rand() % 6 + 1;
+		{			
+			totalDiceNumber = rollDieOne + rollDieTwo;
 			std::cout << "The number of the first die is " << rollDieOne << std::endl;
 			std::cout << "The number of the second die is " << rollDieTwo << std::endl;
+			std::cout << "Your Total Number is " << totalDiceNumber << std::endl;
 		}
 		else if (userChoice == "No" || userChoice == "no")
 		{
-			std::cout << "BE GONE BOI" << std::endl;
+			std::cout << "THEN BE GONE BOI" << std::endl;
 		}
-	} while
-
-
-
+	} while (userChoice != "yes");
 
 	system("pause");
 	return 0;
